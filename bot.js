a1067352539f4aead90daa90a0dc6915d462a81c
Xule Lin <43122877+linxule@users.noreply.github.com>
2024-05-14 09:42:37 +0100

    2024-05-14 09:42

--- bot.js
+++ bot.js
@@ -32,7 +32,6 @@ const client = new Client({
 client.once('ready', async () => {
     await connectToMongo();
     console.log('Bot is ready!');
-    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 // Function to retrieve OpenAI Thread ID using MongoDB
